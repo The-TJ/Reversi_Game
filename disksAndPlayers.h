83d56ca6cefc34005bb0331c3f4b54008ea5c5dd
The-TJ <32796932+The-TJ@users.noreply.github.com>
2018-05-29 16:48:31 +0100

    Update disksAndPlayers.h

--- disksAndPlayers.h
+++ disksAndPlayers.h
@@ -24,3 +24,17 @@
 
     extern player player1;
     extern player player2;
+ 
+    void initializePlayers();
+
+    void initializeBoard(disk board[SIZE][SIZE]);
+
+    void results();
+
+    void printBoard(disk board [SIZE][SIZE]);
+
+    void makemove(disk board[SIZE][SIZE], player Cplayer, int chosenrow, int chosencol, int rivaldiskinquesrow, int rivaldiskinquescol);
+
+    void possiblemoves(disk board[SIZE][SIZE], player Cplayer);
+
+    void gamelogic(disk board[SIZE][SIZE], player player1, player player2);
