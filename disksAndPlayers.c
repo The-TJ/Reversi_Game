42005027fd8911e806e204843f0e6a6070d9bb59
The-TJ <32796932+The-TJ@users.noreply.github.com>
2018-06-08 20:12:45 +0100

    Update disksAndPlayers.c

--- disksAndPlayers.c
+++ disksAndPlayers.c
@@ -121,4 +121,18 @@ else
 }
 void possiblemoves(disk board[SIZE][SIZE], player Cplayer)
 
+{
+    int num=0;
+    int arr[20][4];
+    if(Cplayer.type==BLACK)
+    {
+        for(int i=0; i<SIZE;i++)
+        {
+            for(int j=0; j<SIZE;j++)
+            {
+                switch(board[i][j].type)
+                {
+                    case WHITE: // When we find a rival disk, a series of if statements determine if any of its neighbouring element is a possible move 
+
+                        if(i-1>=0 && j-1>=0)       //Neighbpur position one row above and one coloum to the left
 
