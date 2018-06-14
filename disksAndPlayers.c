30fca41e4e1967f51ea3e920b1107a48f9169d02
The-TJ <32796932+The-TJ@users.noreply.github.com>
2018-06-14 15:36:13 +0100

    Update disksAndPlayers.c

--- disksAndPlayers.c
+++ disksAndPlayers.c
@@ -395,49 +395,5 @@ void possiblemoves(disk board[SIZE][SIZE], player Cplayer)
         }
     }
 }
-else
-    {
-        for(int i=0; i<SIZE;i++)
-        {
-            for(int j=0; j<SIZE;j++)
-            {
-                switch(board[i][j].type)
-                {
-                    case BLACK: // When we find a rival disk, a series of if statements determine if any of its neighbouring element is a possible move 
-
-                        if(i-1>=0 && j-1>=0)       //Neighbpur position one row above and one coloum to the left
-			{
-                            if(board[i-1][j-1].type==NONE)
-                            {
-                                int check=1;
-                                int a=i+1;
-                                int b=j+1;
-                                while(a<SIZE && b<SIZE && check==1)
-                                {
-                                    if(board[a][b].type==WHITE)
-                                    {
-                                    	if((i-1>=0 && i-1<8) && (j-1>=0 && j-1<8))
-                                    	{
-                                        	arr[num][0] = i-1;
-                                        	arr[num][1] = j-1;
-                                        	arr[num][2] = i;
-                                        	arr[num][3] = j;
-                                        	num++;
-                                        	check=0;
-                                        }
-                                        break;
-                                    }
-                                    else if(board[a][b].type==NONE)
-                                        break;
-                                    else if(board[a][b].type==BLACK)
-                                    {
-                                        a+=1;
-                                        b+=1;
-                                    }
-                                }
-                            }
-                        }
-                       
-                        
                         
 
