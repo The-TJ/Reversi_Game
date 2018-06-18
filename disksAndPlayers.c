568bea0c0ebeb40c3f8621c8401dc58fb9859940
The-TJ <32796932+The-TJ@users.noreply.github.com>
2018-06-18 17:40:42 +0100

    Update disksAndPlayers.c

--- disksAndPlayers.c
+++ disksAndPlayers.c
@@ -469,6 +469,284 @@ else
                             }
                         }
                         if(i-1>=0 && j+1<=SIZE)        //Neighbpur position one row above and one coloumn to the right
+			{
+                            if(board[i-1][j+1].type==NONE)
+                            {
+                                int check=1;
+                                int a=i+1;
+                                int b=j-1;
+                                while(a<SIZE && b>=0 && check==1)
+                                {
+                                    if(board[a][b].type==WHITE)
+                                    {
+                                    	if((i-1>=0 && i-1<8) && (j+1>=0 && j+1<8))
+                                    	{
+                                        	arr[num][0]=i-1;
+                                        	arr[num][1]=j+1;
+                                        	arr[num][2] = i;
+                                        	arr[num][3] = j;
+                                        	num++;
+                                        	check=0;
+                                        }
+                                        break;
+                                    }
+                                    else if(board[a][b].type==NONE)
+                                        break;
+                                    else if(board[a][b].type == BLACK)
+                                    {
+                                        a+=1;
+                                        b-=1;
+                                    }
+                                }
+                            }
+                        }
+                        if(j-1>=0)  //Neighbpur position in the same row and one coloum to the left
+                        {
+                            if(board[i][j-1].type==NONE)
+                            {
+                                int check=1;
+                                int a=i;
+                                int b=j+1;
+                                while(b<SIZE && check==1)
+                                {
+                                    if(board[a][b].type==WHITE)
+                                    {
+                                    	if((i>=0 && i<8) && (j-1>=0 && j-1<8))
+                                    	{
+                                       		arr[num][0]=i;
+                                        	arr[num][1]=j-1;
+                                        	arr[num][2] = i;
+                                        	arr[num][3] = j;
+                                        	num++;
+                                        	check=0;
+                                        }
+                                        break;
+                                    }
+                                    else if(board[a][b].type==NONE)
+                                        break;
+                                    else if(board[a][b].type == BLACK)
+                                    {
+                                        b+=1;
+                                    }
+                                }
+                            }
+                        }
+                        if(j+1<=SIZE)            //Neighbpur position in the same row and one coloum to the right
+                        {
+                            if(board[i][j+1].type==NONE)
+                            {
+                                int check=1;
+                                int a=i;
+                                int b=j-1;
+                                while(b>=0 && check==1)
+                                {
+                                    if(board[a][b].type==WHITE)
+                                    {
+                                    	if((i>=0 && i<8) && (j+1>=0 && j+1<8))
+                                    	{
+                                        	arr[num][0]=i;
+                                        	arr[num][1]=j+1;
+                                        	arr[num][2] = i;
+                                        	arr[num][3] = j;
+                                        	num++;
+                                        	check=0;
+                                        }
+                                        break;
+                                    }
+                                    else if(board[a][b].type==NONE)
+                                        break;
+                                    else if(board[a][b].type == BLACK)
+                                    {
+                                        b-=1;
+                                    }
+                                }
+                            }
+                        }
+                        if(i+1>=SIZE && j-1>=0)                //Neighbpur position one row below and one coloum to the left
+                        {
+                            if(board[i+1][j-1].type==NONE)
+                            {
+                                int check=1;
+                                int a=i-1;
+                                int b=j+1;
+                                while(a<SIZE && b>=0 && check==1)
+                                {
+                                    if(board[a][b].type==WHITE)
+                                    {
+                                    	if((i+1>=0 && i+1<8) && (j-1>=0 && j-1<8))
+                                    	{
+                                        	arr[num][0]=i+1;
+                                        	arr[num][1]=j-1;
+                                        	arr[num][2] = i;
+                                        	arr[num][3] = j;
+                                        	num++;
+                                        	check=0;
+                                        }
+                                        break;
+                                    }
+                                    else if(board[a][b].type==NONE)
+                                        break;
+                                    else if(board[a][b].type == BLACK)
+                                    {
+                                        a+=1;
+                                        b-=1;
+                                    }
+                                }
+                            }
+                        }
+                        if(i+1<=SIZE)            //Neighbpur position one row below and in the same coloumn
+                        {
+                            if(board[i+1][j].type==NONE)
+                            {
+                                int check=1;
+                                int a=i-1;
+                                int b=j;
+                                while(a>=0 && check==1)
+                                {
+                                    if(board[a][b].type==WHITE)
+                                    {
+                                    	if((i+1>=0 && i+1<8) && (j>=0 && j<8))
+                                    	{
+                                        	arr[num][0]=i+1;
+                                        	arr[num][1]=j;
+                                        	arr[num][2] = i;
+                                        	arr[num][3] = j;
+                                        	num++;
+                                        	check=0;
+                                        }
+                                        break;
+                                    }
+                                    else if(board[a][b].type==NONE)
+                                        break;
+                                    else if(board[a][b].type == BLACK)
+                                    {
+                                        a-=1;
+                                    }
+                                }
+                            }
+                        }
+                        if(i+1<=SIZE && j+1<=SIZE)              //Neighbpur position one row below and one coloum to the right
+                        {
+                            if(board[i+1][j+1].type==NONE)
+                            {
+                                int check=1;
+                                int a=i-1;
+                                int b=j-1;
+                                while(a>=0 && b>=0 && check==1)
+                                {
+                                    if(board[a][b].type==WHITE)
+                                    {
+                                    	if((i+1>=0 && i+1<8) && (j+1>=0 && j+1<8))
+                                    	{
+                                        	arr[num][0]=i+1;
+                                        	arr[num][1]=j+1;
+                                        	arr[num][2] = i;
+                                        	arr[num][3] = j;
+                                        	num++;
+                                        	check=0;
+                                        }
+                                        break;
+                                    }
+                                    else if(board[a][b].type==NONE)
+                                        break;
+                                    else if(board[a][b].type == BLACK)
+                                    {
+                                        a-=1;
+                                        b-=1;
+                                    }
+                                }
+                            }
+                        }
+                        break;
+                    default:
+                        break;
+                }
+            }
+        }
+    }
+    int gameend=0;
+    if(num==0)
+    {
+        gameend++;
+        if(gameend==2)
+        {
+            results();
+        }
+    }
+    else{
+    gameend=0;
+    int count=1;
+    int arr2[num][4];
+    arr2[0][0]= arr[0][0];//removing duplicates by assigning them to a new array, only one version of the same move
+    arr2[0][1]= arr[0][1];
+    arr2[0][2]= arr[0][2];
+    arr2[0][3]= arr[0][3];
+    for(int i=1;i<num;i++)
+    {
+        if(i>=2)
+        {
+            if(((arr[i][0] == arr2[i-1][0]) && (arr[i][1] == arr2[i-1][1])) || ((arr[i][0] == arr2[i-2][0]) && (arr[i][1] == arr2[i-2][1])))
+                continue;
+            else
+            {
+                arr2[count][0]= arr[i][0];
+                arr2[count][1]= arr[i][1];
+                arr2[count][2]= arr[i][2];
+                arr2[count][3]= arr[i][3];
+                count++;
+            }
+        }
+        else
+        {
+            if((arr[i][0] == arr2[i-1][0]) && (arr[i][1] == arr2[i-1][1]))
+                continue;
+            else
+            {
+                arr2[count][0]= arr[i][0];
+                arr2[count][1]= arr[i][1];
+                arr2[count][2]= arr[i][2];
+                arr2[count][3]= arr[i][3];
+                count++;
+            }
+        }
+    }
+    printf("\nIt's %s's turn\n", Cplayer.name );
+    printf("\nList of Possible moves -\n\n");
+
+    //Loop to print the list of possible moves
+    for (int i = 0; i < count; ++i)
+    {
+        for (int j = 0; j < 2; ++j)
+        {
+            if(j==0)
+            {
+                printf("%d. (%d , ",i+1,arr2[i][j]+1 ); 
+            }
+            else
+            {
+                printf("%d)",arr2[i][j]+1 ); 
+            }
+        }
+        printf("\n");
+    }
+    int choices, chosenrow,chosencol, rivaldiskinquesrow, rivaldiskinquescol;
+    printf("\nTo chose a move enter the number in front of it\n");
+    scanf("%d", &choices);
+    if(choices>0 && choices<count+1)
+    {
+        rivaldiskinquesrow = arr2[choices-1][2];
+        rivaldiskinquescol = arr2[choices-1][3];
+        chosenrow = arr2[choices-1][0];
+        chosencol = arr2[choices-1][1];
+        makemove(board, Cplayer, chosenrow, chosencol,rivaldiskinquesrow, rivaldiskinquescol);
+    }
+    else{
+        printf("\nInvalid choice Entered, please enter again\n"); //this is printed if the user enters an invalid option
+        printBoard(board);
+        possiblemoves(board, Cplayer);
+    }
+}
+}
 
                         
 
