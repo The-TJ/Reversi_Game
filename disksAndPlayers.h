8b01472420e5cd80a6069d9440eb1ecf6dbd87ac
The-TJ <32796932+The-TJ@users.noreply.github.com>
2018-06-22 20:19:15 +0100

    Update disksAndPlayers.h

--- disksAndPlayers.h
+++ disksAndPlayers.h
@@ -1,4 +1,4 @@
- #define SIZE 8
+ #define SIZE 
  #define len 10
 
    typedef struct position{
@@ -10,7 +10,7 @@
     enum colour{
         WHITE,
         BLACK,
-        NONE
+        NONEp,
     };
 
     typedef struct disk{
